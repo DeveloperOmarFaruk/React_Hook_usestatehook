976c07aec1e028a0564f7c03d0d9c8692bca0c7c
Omar Faruk <omarfaruk18115@gmail.com>
2021-03-08 19:55:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 function App() {
 
-  const [myName, setMyName] = useState('Developer Omar Faruk')
+  const [myName, setMyName] = useState('MERN Developer Omar Faruk')
 
   const changeName = () =>{
-  
-    setMyName('MERN Developer Omar Faruk')
+    
+    let val = myName;
+    (val === 'MERN Developer Omar Faruk') ? 
+    setMyName('FrontEnd Developer Omar Faruk') : setMyName('MERN Developer Omar Faruk')
   }
 
   return (
